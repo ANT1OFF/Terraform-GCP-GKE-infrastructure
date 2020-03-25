e25221dba272f151a13e7dcc16e45ead034e9291
Erlend Fonnes <erlend.fonnes@gmail.com>
2020-03-25 23:05:21 +0100

    Cleaned up scripts

--- terraform/scripts/tf-apply.sh
+++ terraform/scripts/tf-apply.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run from any folder in or bellow the main terraform folder of the repo.
 # The script takes one argument: the path to the file containing environment varialbes to be injected before running the Terraform configuration.
-# The name of the env file defaults to "env.txt" inside the scripts folder of this repository.
+# The name of the env file defaults to the terraform.tfvars file inside the scripts folder of this repository.
 
-# The script loads all vars in env.txt,
+# The script passes the envfile as a var-file,
 # plans and applies all terraform configs in dirlist.
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -26,7 +26,6 @@ sprint () {
     echo
 }
 
-#TODO: make function return status code instead of exiting
 tf-apply () {
     sprint "Running terrafom plan"
 
@@ -62,11 +61,11 @@ import-argo-state () {
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-# SCRIPT
+# Setup
 # ---------------------------------------------------------------------------------------------------------------------
 
-# trying to find the main terraform folder
 # TODO: merely checking that the folder is named "terraform" isn't very robust. mby fix?
+# Trying to find the main terraform folder of the repo
 dir=$(basename "$(pwd)")
 while [ "$dir" != "terraform" ] && [ "$dir" != "/" ]
 do
@@ -80,14 +79,17 @@ then
     exit 1
 fi
 
-basedir=$(pwd)
 
+# basedir contains the path to the main terraform folder of the repo
+basedir=$(pwd)
+# envfile should be a terraform tfvars file containing variables
 envfile="$1"
 
-# if env not provided
+
+# Checking if envfile is provided
 if [ -z "$envfile" ]
 then
-    # defaults to a "env.txt" inside the scripts folder.
+    # Defaults to the terraform.tfvars file inside the scripts folder.
     envfile="${basedir}/scripts/terraform.tfvars"
 fi
 
@@ -106,12 +108,14 @@ for tfdir in $dirlist
 do
     echo "Moving to $tfdir"
     cd "$basedir$tfdir" || { echo "Could not cd, exiting"; exit 1; }
+
     if [ "$tfdir" = "/dev/argo-2" ]
     then
-        set +e  # turn off error-trapping
+        set +e  # Turn off error-trapping
         tf-apply || { echo "tf-apply failed, running state import"; import-argo-state;  }
-        set -e  # turn on error-trapping
+        set -e  # Turn on error-trapping
     fi
+
     echo "running tf-apply"
     tf-apply || { echo "tf-apply failed, exiting"; exit 1; }
 done
