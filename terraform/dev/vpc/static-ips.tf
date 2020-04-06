d4870b7b1c23c6baa3c0ee646d48d1ea7392077f
Johan <johanselnes2013@gmail.com>
2020-04-06 03:19:45 +0200

    cleanup

--- terraform/dev/vpc/static-ips.tf
+++ terraform/dev/vpc/static-ips.tf
@@ -1,10 +1,9 @@
-#resource "google_compute_global_address" "app-ip" {
-#    name = "app-static-ip-address"
-#}
 
-#resource "google_compute_global_address" "argo-ip" {
-#    name = "argo-static-ip-address"
-#}
+resource "google_dns_managed_zone" "fonn" {
+  name     = "fonn-zone"
+  dns_name = "fonn.es."
+}
+
 
 resource "google_dns_record_set" "frontend" {
   name = "frontend.${google_dns_managed_zone.fonn.dns_name}"
@@ -29,19 +28,3 @@ resource "google_compute_address" "app-ip" {
   region       = var.region
 }
 
-
-
-#resource "google_dns_record_set" "argo" {
-#  name = "argo.${google_dns_managed_zone.fonn.dns_name}"
-#  type = "A"
-#  ttl  = 300
-#
-#  managed_zone = google_dns_managed_zone.fonn.name
-#
-#  rrdatas = [google_compute_global_address.argo-ip.address]
-#}
-
-resource "google_dns_managed_zone" "fonn" {
-  name     = "fonn-zone"
-  dns_name = "fonn.es."
-}
