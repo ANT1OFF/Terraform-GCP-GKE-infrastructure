6eb3101c0efa4a5f89a8e8af23723315704fb41a
Johan <johanselnes2013@gmail.com>
2020-05-09 10:08:11 +0200

    adding enable services script

---
+++ terraform/scripts/enable_services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gcloud services enable compute.googleapis.com
+gcloud services enable container.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable containerregistry.googleapis.com
+gcloud services enable dns.googleapis.com
+gcloud services enable cloudprofiler.googleapis.com
+gcloud services enable stackdriver.googleapis.com
+gcloud services enable monitoring.googleapis.com
+gcloud services enable logging.googleapis.com
+gcloud services enable cloudtrace.googleapis.com
+gcloud services enable clouderrorreporting.googleapis.com
+gcloud services enable sqladmin.googleapis.com
